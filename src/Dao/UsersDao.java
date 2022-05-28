d6b728bda0fb73c71064a14a9f2ba2ccede15339
Anwan <2730340037@qq.com>
2022-05-28 10:21:26 +0800

    add:until + userDao

---
+++ src/Dao/UsersDao.java
@@ -0,0 +1,64 @@
+package Dao;
+
+import until.DBUtil;
+
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class UsersDao {
+    public String name;
+    public String password;
+    protected String confirmpassword;
+    protected String mail;
+
+    public void setname(String name) {
+        this.name = name;
+    }
+    public void setmail(String mail) {
+        this.mail = mail;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setconfirmpasswd(String confirmpassword) {
+        this.confirmpassword = confirmpassword;
+    }
+    public boolean JudgeRegister() throws SQLException, ClassNotFoundException {
+        if (this.name.equals("")) {
+            JOptionPane.showMessageDialog(null, "用户名不能为空！", "用户名", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+        if (this.password.equals("")) {
+            JOptionPane.showMessageDialog(null, "密码不能为空！", "密码为空", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+        if (!this.password.equals(this.confirmpassword)) {
+            JOptionPane.showMessageDialog(null, "两次输入的密码不一致!", "密码不一致", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+        //符合规则，弹出注册成功的窗口，并将账号添加数据库
+        JOptionPane.showMessageDialog(null, "注册成功");
+        userAdd();
+        return true;
+    }
+    public void userAdd() throws ClassNotFoundException, SQLException {
+        String sql = "INSERT INTO ttest (name,password)" + "VALUES (?,?)";
+        Connection conn = null;
+        try {
+            conn = DBUtil.getConn();
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, this.name);
+            ps.setString(2, this.password);
+            ps.executeUpdate();
+            ps.close();
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println("添加用户失败！");
+        }
+    }
+
+
+}
+
---
+++ src/until/DBUtil.java
@@ -0,0 +1,51 @@
+package until;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DBUtil {
+    public static final String url="jdbc:mysql://localhost:3306/servers";
+    public static final String user ="root";
+    public static final String password="shhk010527..";
+    //类第一次加载的时候执行一次
+    static {
+        try {
+            //1.加载驱动
+            Class.forName("com.mysql.cj.jdbc.Driver");
+//            System.out.println("驱动加载成功");
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }//有可能报错
+    }
+    //2.数据库链接
+    public static Connection getConn() {
+        Connection conn =null;
+        try {
+            conn= DriverManager.getConnection(url, user, password);
+//           System.out.println("数据库加载成功");
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            System.out.println("数据库加载失败");
+        }return conn;
+    }
+    public static void close(Connection conn, PreparedStatement pst) {
+        try {
+            //5.关闭数据库
+            conn.close();
+            pst.close();
+
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+//    public static void main(String[] args) {
+//        Connection conn=DBUtil.getConn();
+//        System.out.println(conn);
+
+}
